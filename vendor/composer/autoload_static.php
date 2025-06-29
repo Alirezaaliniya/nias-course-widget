6161447348c636d796041c3ab6fbf60311ab9d64
Alireza aliniya <Alireza.aliniya79@gmail.com>
2025-06-29 20:08:56 +0330

    Add PDF Reader and Page Management Classes
    - Introduced Rectangle class for representing rectangles in PDF.
    - Implemented Page class to manage PDF pages, including attributes and boundaries.
    - Added PageBoundaries class for defining standard page boundary constants.
    - Created PdfReader class for reading PDF documents and managing page counts.
    - Developed PdfReaderException for handling PDF reading errors.
    - Enhanced Tcpdf integration with Fpdi class for importing PDF pages into TCPDF.
    - Added FpdfTpl class for compatibility with tFPDF.
    - Implemented autoload functionality for FPDI classes.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -92,19 +92,6 @@ class ComposerStaticInit1aa62b11b2fbcfda9fa8ef6dadc4f527
 
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'Datamatrix' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/barcodes/datamatrix.php',
-        'FPDF' => __DIR__ . '/..' . '/setasign/fpdf/fpdf.php',
-        'PDF417' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/barcodes/pdf417.php',
-        'QRcode' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/barcodes/qrcode.php',
-        'TCPDF' => __DIR__ . '/..' . '/tecnickcom/tcpdf/tcpdf.php',
-        'TCPDF2DBarcode' => __DIR__ . '/..' . '/tecnickcom/tcpdf/tcpdf_barcodes_2d.php',
-        'TCPDFBarcode' => __DIR__ . '/..' . '/tecnickcom/tcpdf/tcpdf_barcodes_1d.php',
-        'TCPDF_COLORS' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_colors.php',
-        'TCPDF_FILTERS' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_filters.php',
-        'TCPDF_FONTS' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_fonts.php',
-        'TCPDF_FONT_DATA' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_font_data.php',
-        'TCPDF_IMAGES' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_images.php',
-        'TCPDF_STATIC' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_static.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
